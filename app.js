1b3b971be8dabcd491198bc3a39af4bc553dc2b5
infernalslam <infernal-slam@hotmail.com>
2017-07-16 20:46:40 +0700

    javascript

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('...... Program Calculatetor .......')
+function add (x, y) {
+  return x + y
+}
+
+function sub () {
+  var x = 20
+  var y = 5
+  return x - y
+}
+
+function mul (x, y) {
+  return x * y
+}
+
+function de (x, y) {
+  return x / y
+}
+
+add(10, 5)
+console.log(add(10, 5))
+sub()
+mul(5, 10)
+de(10, 5)
